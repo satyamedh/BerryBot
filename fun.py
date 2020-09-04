eca2333df1ddd2eed1aca610a3eea01e13fcb9ce
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2020-09-04 21:34:05 +0530

    LOL I LEAKED MY TOKEN

--- fun.py
+++
@@ -1,5 +0,0 @@
-listo = ['Satyamedh!']
-
-while True:
-    listo = [listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo, listo]
-    print(listo)
