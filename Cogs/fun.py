9b777db346caefbb6dbcf902353c4ae84fe85fce
satyamedh hulyalkar <satyamedh9@gmail.com>
2021-03-04 18:24:04 +0530

    moved choose command to fun cog

---
+++ Cogs/fun.py
@@ -0,0 +1,13 @@
+import random
+
+from discord.ext import commands
+
+
+class FunCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def choose(self, ctx, *, opts=None):
+        opts = opts.split(',')
+        await ctx.send(f'I Choose:`{random.choice(opts)}`')
