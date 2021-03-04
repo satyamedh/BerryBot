b13a5cf4ec40869918aef309b8897877d7c3f48b
satyamedh hulyalkar <satyamedh9@gmail.com>
2021-03-04 18:27:58 +0530

    moved memban and memkick cmds to fun.py

--- Cogs/fun.py
+++ Cogs/fun.py
@@ -1,5 +1,5 @@
 import random
-
+import discord
 import asyncio
 from discord.ext import commands
 
@@ -12,5 +12,24 @@ class FunCog(commands.Cog):
     async def choose(self, ctx, *, opts=None):
         opts = opts.split(',')
         await ctx.send(f'I Choose:`{random.choice(opts)}`')
+
+    @commands.command()
+    async def memkick(self, ctx, member: discord.Member):
+        await ctx.send(f'kicked {member.mention}!')
+        await member.send(f'{ctx.author.mention} Wanted to kick you lolololol. server:`{ctx.guild.name}`')
+        await asyncio.sleep(5)
+        await ctx.send(f'jk I didn\'t kick {member.mention}')
+
+    @commands.command()
+    async def memban(self, ctx, member: discord.Member):
+        await ctx.send(f'BANNED {member.mention}!')
+        await member.send(f'{ctx.author.mention} Wanted to ban you lolololol. server:`{ctx.guild.name}`')
+        await asyncio.sleep(10)
+        await ctx.send(f'jk I didn\'t BAN {member.mention} from existance')
+
+
+
+
+
 
 
