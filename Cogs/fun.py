b8cb2551052a8aa51bf3ba9ac5dfd31f0691a1ed
satyamedh hulyalkar <satyamedh9@gmail.com>
2021-03-05 10:08:39 +0530

    moved 8ball to fun.py

--- Cogs/fun.py
+++ Cogs/fun.py
@@ -25,8 +25,31 @@ class FunCog(commands.Cog):
         await ctx.send(f'BANNED {member.mention}!')
         await member.send(f'{ctx.author.mention} Wanted to ban you lolololol. server:`{ctx.guild.name}`')
         await asyncio.sleep(10)
-        await ctx.send(f'jk I didn\'t BAN {member.mention} from existance')
+        await ctx.send(f'jk I didn\'t BAN {member.mention}')
 
+    @commands.command(aliases=['8ball'])
+    async def _8ball(self, ctx, *, question):
+        responses = ["It is certain.",
+                     "It is decidedly so.",
+                     "Without a doubt.",
+                     "Yes - definitely.",
+                     "You may rely on it.",
+                     "As I see it, yes.",
+                     "Most likely.",
+                     "Affirmative.",
+                     "Yeah.",
+                     "I'm sure.",
+                     "Reply hazy, try again.",
+                     "Ask again later.",
+                     "Better not tell you now.",
+                     "Cannot predict now.",
+                     "Concentrate and ask again.",
+                     "Don't count on it.",
+                     "My reply is no.",
+                     "My sources say no.",
+                     "Umm...No.",
+                     "Very doubtful."]
+        await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 
 
--- main.py
+++ main.py
@@ -284,29 +284,6 @@ async def remove(ctx, game, code):
         await ctx.send('bruh the game dosen\'t exists lolololololol')
 
 
-@bot.command(aliases=['8ball'])
-async def _8ball(ctx, *, question):
-    responses = ["It is certain.",
-                 "It is decidedly so.",
-                 "Without a doubt.",
-                 "Yes - definitely.",
-                 "You may rely on it.",
-                 "As I see it, yes.",
-                 "Most likely.",
-                 "Affirmative.",
-                 "Yeah.",
-                 "I'm sure.",
-                 "Reply hazy, try again.",
-                 "Ask again later.",
-                 "Better not tell you now.",
-                 "Cannot predict now.",
-                 "Concentrate and ask again.",
-                 "Don't count on it.",
-                 "My reply is no.",
-                 "My sources say no.",
-                 "Umm...No.",
-                 "Very doubtful."]
-    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 
 @bot.command()
